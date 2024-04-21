21c9905c65223b69a56a478eb0b39f349b9e6840
Vibhu Yadav <vibhu_yadav@apple.com>
2024-04-21 11:12:39 +0530

    updated code added skills section

---
+++ src/components/Skills.jsx
@@ -0,0 +1,65 @@
+
+export const Skills = () => {
+
+    return (
+        <div class="max-w-lg mt-3 text-xl leading-relaxed text-gray-600 dark:text-gray-400 md:mt-8">
+    <div class="flex flex-wrap justify-normal">
+        <div class="flex justify-center items-center m-1 font-medium py-1 px-2 rounded-full text-orange-100 bg-orange-700 border border-orange-700 ">
+            <div class="text-xs font-normal leading-none max-w-full flex-initial">java</div>
+        </div>
+        <div class="flex justify-center items-center m-1 font-medium py-1 px-2 rounded-full text-orange-100 bg-orange-700 border border-orange-700 ">
+            <div class="text-xs font-normal leading-none max-w-full flex-initial">kotlin</div>
+        </div>
+        <div class="flex justify-center items-center m-1 font-medium py-1 px-2 rounded-full text-orange-100 bg-orange-700 border border-orange-700 ">
+            <div class="text-xs font-normal leading-none max-w-full flex-initial">php</div>
+        </div>
+        <div class="flex justify-center items-center m-1 font-medium py-1 px-2 rounded-full text-orange-100 bg-orange-700 border border-orange-700 ">
+            <div class="text-xs font-normal leading-none max-w-full flex-initial">javascript</div>
+        </div>
+        <div class="flex justify-center items-center m-1 font-medium py-1 px-2 rounded-full text-orange-100 bg-orange-700 border border-orange-700 ">
+            <div class="text-xs font-normal leading-none max-w-full flex-initial">python</div>
+        </div>
+        <div class="flex justify-center items-center m-1 font-medium py-1 px-2 rounded-full text-orange-100 bg-orange-700 border border-orange-700 ">
+            <div class="text-xs font-normal leading-none max-w-full flex-initial">react</div>
+        </div>
+        <div class="flex justify-center items-center m-1 font-medium py-1 px-2 rounded-full text-orange-100 bg-orange-700 border border-orange-700 ">
+            <div class="text-xs font-normal leading-none max-w-full flex-initial">C++</div>
+        </div>
+        <div class="flex justify-center items-center m-1 font-medium py-1 px-2 rounded-full text-orange-100 bg-orange-700 border border-orange-700">
+            <div class="text-xs font-normal leading-none max-w-full flex-initial">Nodejs</div>
+        </div>
+        <div class="flex justify-center items-center m-1 font-medium py-1 px-2 rounded-full text-orange-100 bg-orange-700 border border-orange-700 ">
+            <div class="text-xs font-normal leading-none max-w-full flex-initial">graphQL</div>
+        </div>
+        <div class="flex justify-center items-center m-1 font-medium py-1 px-2 rounded-full text-orange-100 bg-orange-700 border border-orange-700 ">
+            <div class="text-xs font-normal leading-none max-w-full flex-initial">gRPC</div>
+        </div>
+        <div class="flex justify-center items-center m-1 font-medium py-1 px-2 rounded-full text-orange-100 bg-orange-700 border border-orange-700 ">
+            <div class="text-xs font-normal leading-none max-w-full flex-initial">Redis</div>
+        </div>
+        <div class="flex justify-center items-center m-1 font-medium py-1 px-2 rounded-full text-orange-100 bg-orange-700 border border-orange-700 ">
+            <div class="text-xs font-normal leading-none max-w-full flex-initial">Mysql</div>
+        </div>
+        <div class="flex justify-center items-center m-1 font-medium py-1 px-2 rounded-full text-orange-100 bg-orange-700 border border-orange-700 ">
+            <div class="text-xs font-normal leading-none max-w-full flex-initial">Kafka</div>
+        </div>
+        <div class="flex justify-center items-center m-1 font-medium py-1 px-2 rounded-full text-orange-100 bg-orange-700 border border-orange-700 ">
+            <div class="text-xs font-normal leading-none max-w-full flex-initial">Elasticseacrh</div>
+        </div>
+        <div class="flex justify-center items-center m-1 font-medium py-1 px-2 rounded-full text-orange-100 bg-orange-700 border border-orange-700 ">
+            <div class="text-xs font-normal leading-none max-w-full flex-initial">AWS</div>
+        </div>
+        <div class="flex justify-center items-center m-1 font-medium py-1 px-2 rounded-full text-orange-100 bg-orange-700 border border-orange-700 ">
+            <div class="text-xs font-normal leading-none max-w-full flex-initial">docker</div>
+        </div>
+        <div class="flex justify-center items-center m-1 font-medium py-1 px-2 rounded-full text-orange-100 bg-orange-700 border border-orange-700 ">
+            <div class="text-xs font-normal leading-none max-w-full flex-initial">opentelemetry</div>
+        </div>
+        <div class="flex justify-center items-center m-1 font-medium py-1 px-2 rounded-full text-orange-100 bg-orange-700 border border-orange-700 ">
+            <div class="text-xs font-normal leading-none max-w-full flex-initial">grafana</div>
+        </div>
+    </div>
+</div>
+    );
+
+}
